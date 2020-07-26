b740cbd003a45698979447d7ef6cc748ff454e4b
Vasileios Pasialiokis <whiterocket@outlook.com>
2020-07-27 01:34:01 +0300

    version bump

--- userscripts/ao3_wrangling_shortcuts.js
+++ userscripts/ao3_wrangling_shortcuts.js
@@ -11,7 +11,7 @@
 // @match	https://archiveofourown.org/*
 // @match	http://insecure.archiveofourown.org/*
 //
-// @version	0.3.2
+// @version	0.4.0
 // @updateURL	https://raw.githubusercontent.com/vaaas/ao3_wrangling_scripts/master/userscripts/ao3_wrangling_shortcuts.js
 // ==/UserScript==
 
