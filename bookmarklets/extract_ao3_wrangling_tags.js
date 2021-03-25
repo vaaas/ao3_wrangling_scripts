e2be71c3b78cab3e992645389f501078d62da523
Vasileios Pasialiokis <whiterocket@outlook.com>
2021-03-25 05:00:26 +0200

    update extract tags script

--- bookmarklets/extract_ao3_wrangling_tags.js
+++ bookmarklets/extract_ao3_wrangling_tags.js
@@ -1,43 +1,46 @@
 (function(){
-	const get = url => new Promise(resolve => {
-		const req = new XMLHttpRequest()
-		req.onload = x => resolve(req.responseText)
-		req.open("GET", url)
-		req.send() })
+	'use strict'
+	const tap = f => x => { f(x) ; return x }
+	const pluck = k => x => x[k]
+	const map = f => function* (xs) { let i = 0 ; for (const x of xs) yield f(x, i++, xs) }
+	const reduce = f => i => xs => { let a = i ; for (const x of xs) a = f(x)(a) ; return a }
+	const I = x => x
+	const qs = x => d => d.querySelector(x)
+	const qss = x => d => d.querySelectorAll(x)
+	const compl = (...fs) => x => reduce(I)(x)(fs)
+	const business = compl(qss('tr th[title="tag"] label'), map(pluck('innerHTML')))
+	const get_next = qs('a[rel="next"]')
+	const sleep = x => new Promise(ok => setTimeout(ok, x))
+	const is_sunday = () => new Date().getDay() === 0
+	const is_nf = () => RegExp('^/tags/No Fandom/wrangle').test(location.pathname)
+	const parser = new DOMParser()
+	const parse = x => parser.parseFromString(x, 'text/html')
+	const text = x => x.text()
+	const before = p => tap(x => p.parentElement.insertBefore(x, p))
+	const elem = x => document.createElement(x)
 
-	const pluck = attr => obj => obj[attr]
+	async function* results_generator(x)
+		{ let url = x
+		let c = 0
+		while (url !== null)
+			{ yield* await fetch(url)
+				.then(text)
+				.then(parse)
+				.then(tap(x => url = get_next(x)))
+				.then(business)
+			await sleep(c++ < 1000 ? 3e3 : 10e3) }}
 
-	const business = dom => Array.from(dom.querySelectorAll("tr th[title='tag'] label")).map(pluck("innerHTML"))
-
-	const get_next = dom => dom.querySelector("a[rel='next']")
-	
-	const sleep = ms => new Promise(resolve => setTimeout(resolve, ms))
-
-	const is_sunday = () => (new Date).getDay() === 0
-
-	const is_nf = () => RegExp("^/tags/No Fandom/wrangle").test(location.pathname)
-
-	async function main(url) {
-		if (is_sunday())
+	async function main()
+		{ if (is_sunday())
 			return alert("Don't run this script on Sundays")
 		else if (is_nf())
-			return alert("Don't run this on No Fandom")
-		let counter = 1
-		const parser = new DOMParser()
-		const results = open()
-		const doc = results.document
-		doc.write("<meta charset='utf-8'><pre>")
-		while (url !== null) {
-			doc.title = `${counter} pages deep`
-			counter++
-			const dom = parser.parseFromString(await get(url), "text/html")
-			doc.write(business(dom).join("\n") + "\n")
-			const next = get_next(dom)
-			url = next ? next.href : null
-			await sleep(counter < 1000 ? 3000 : 10000) }
-		doc.write("</pre>")
-		doc.title = "Done!"
-		alert("Done!") }
-	
-	main(location.href)
+			return alert("Don't run this script on No Fandom")
+		const results = before(qs('table')(document))(elem('textarea'))
+		document.title = 'Fetching...'
+		for await (const x of results_generator(location.href))
+			results.value += '\n' + x
+		document.title = 'Done!'
+		alert('Done!') }
+
+	main()
 })()
