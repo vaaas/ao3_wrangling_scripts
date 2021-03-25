a4254ea4506be9e572b149937bed0dd71c1209ff
Vasileios Pasialiokis <whiterocket@outlook.com>
2021-03-25 06:16:37 +0200

    compactification

--- bookmarklets/build/go_to_admin.js
+++ bookmarklets/build/go_to_admin.js
@@ -1,1 +1,1 @@
-javascript:(function(){const n=location.pathname.match(new RegExp("^/users/([^/]+)"));if(n[1])location.pathname=`/admin/users/${n[1]}`})();
+javascript:!function(){const n=location.pathname.match(new RegExp("^/users/([^/]+)"));n[1]&&(location.pathname=`/admin/users/${n[1]}`)}();
