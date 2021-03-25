6cfaa370843c0e0de2935db29093e002291c99be
Vasileios Pasialiokis <whiterocket@outlook.com>
2021-03-25 05:42:43 +0200

    cleanup

--- bookmarklets/extract_ao3_wrangling_tags.js
+++ bookmarklets/extract_ao3_wrangling_tags.js
@@ -1,14 +1,13 @@
 (function(){
 	'use strict'
-	const tap = f => x => { f(x) ; return x }
+	const tap = f => async (x) => { await f(x) ; return x }
 	const pluck = k => x => x[k]
 	const map = f => function* (xs) { let i = 0 ; for (const x of xs) yield f(x, i++, xs) }
-	const reduce = f => i => xs => { let a = i ; for (const x of xs) a = f(x)(a) ; return a }
+	const reduce = f => i => async (xs) => { let a = i ; for await (const x of xs) a = await f(x)(a) ; return a }
 	const I = x => x
 	const qs = x => d => d.querySelector(x)
 	const qss = x => d => d.querySelectorAll(x)
-	const compl = (...fs) => x => reduce(I)(x)(fs)
-	const business = compl(qss('tr th[title="tag"] label'), map(pluck('innerHTML')))
+	const pipe = async (x, ...fs) => reduce(I)(x)(fs)
 	const get_next = qs('a[rel="next"]')
 	const sleep = x => new Promise(ok => setTimeout(ok, x))
 	const is_sunday = () => new Date().getDay() === 0
@@ -20,25 +19,24 @@
 	const elem = x => document.createElement(x)
 
 	async function* results_generator(x)
-		{ let url = x
-		let c = 0
-		while (url !== null)
-			{ yield* await fetch(url)
-				.then(text)
-				.then(parse)
-				.then(tap(x => url = get_next(x)))
-				.then(business)
-			await sleep(c++ < 1000 ? 3e3 : 10e3) }}
+		{ let url = x, c = 0
+		while (url !== null) yield* await pipe
+			(url, fetch, text, parse,
+			tap(x => url = get_next(x)),
+			qss('tr th[title="tag"] label'),
+			map(pluck('innerHTML')),
+			tap(() => sleep(c++ < 1000 ? 3e3 : 10e3))) }
 
 	async function main()
 		{ if (is_sunday())
 			return alert("Don't run this script on Sundays")
 		else if (is_nf())
 			return alert("Don't run this script on No Fandom")
-		const results = before(qs('table')(document))(elem('textarea'))
 		document.title = 'Fetching...'
-		for await (const x of results_generator(location.href))
-			results.value += '\n' + x
+		await pipe
+			(elem('textarea'),
+			before(qs('table')(document)),
+			e => reduce(x => tap(e => e.value += '\n' + x))(e)(results_generator(location.href)))
 		document.title = 'Done!'
 		alert('Done!') }
 
